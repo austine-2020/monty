542400c809ec05cb35a8516f1ad266bf33a627c9
austine-2020 <ogston02@gmail.com>
2024-01-21 21:08:08 -0800

    0x19. C - Stacks, Queues - LIFO, FIFO

---
+++ stack.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+/**
+ * f_stack - fstack prints the top
+ * @counter: line_number
+ * @head: stack head
+ * Return: no return
+ */
+void f_stack(stack_t **head, unsigned int counter)
+{
+	(void)head;
+	(void)counter;
+	bus.lifi = 0;
+}
